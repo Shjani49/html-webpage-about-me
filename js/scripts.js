fb12ba7eb1cce8c372d28d02acfa6453c5b8fb72
Shjani49 <shjani49@gmail.com>
2020-06-29 15:44:42 -0600

    Added Example of JavaScript(scripts.js)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
 console.log( "Hello,Shivani!" );
+// prompt(); = Console.ReadLine(); in C#
+let userInput = prompt("please Enter a number to be cube:");
+// Console.log(); = Console.WriteLine(); in C#
+console.log(userInput ** 3);
